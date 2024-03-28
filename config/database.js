65f6212f83ddb2bf8573ecad4f3a32219f77eb86
ItsMeAldy <rizaldimustakim10@gmail.com>
2024-03-28 21:45:58 +0800

    revisi

--- config/database.js
+++ config/database.js
@@ -10,3 +10,20 @@ module.exports = {
       port : process.env.DB_PORT,
     }
 }
+
+
+// "test": {
+//   "username": "root",
+//   "password": null,
+//   "database": "database_test",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql"
+// },
+
+// "production": {
+//   "username": "root",
+//   "password": null,
+//   "database": "database_production",
+//   "host": "127.0.0.1",
+//   "dialect": "mysql"
+// }
